bf0c00d112c14a5a63ede200ed4896ee9822552d
Sergio Araki <sergioaraki@gmail.com>
2015-07-30 19:26:24 -0300

    Agregada función para listar todas las categorías

--- js/utils.js
+++ js/utils.js
@@ -11,25 +11,39 @@ CTS.Utils = {
     setStatus : function (id,selectName) {
         var self = this;
         jQuery.ajax({
-            type: "POST",
+            type: "GET",
             url: "api/getStatus.php",
             cache: false,
             success: function (listStatus) {        
                 $('#'+selectName).empty();
                  
-                for (var i=0;i<listStatus.length;i++)
-                {
+                for (var i=0;i<listStatus.length;i++) {
                     var selected = "";
                     
                     if (id==listStatus[i].id)
-                    {
-                        selected = "selected"
-                    }
+                        selected = "selected";
 
-                    $('#'+selectName).append(
-                        '<option '+ selected +'  value='  + listStatus[i].id +'>' + listStatus[i].name +'</option>'
-                    );
+                    $('#'+selectName).append('<option '+ selected +'  value='  + listStatus[i].id +'>' + listStatus[i].name +'</option>');
                 }
+            },
+            error: function () {
+                self.showDialog(BootstrapDialog.TYPE_WARNING,"Error","Ha ocurrido un error, intente nuevamente.");
+            }
+        });    
+    },
+    getCategories : function (selectName) {
+        var self = this;
+        jQuery.ajax({
+            type: "GET",
+            url: "api/getCategory.php",
+            cache: false,
+            success: function (listCategory) {        
+                $('#'+selectName).empty();
+                 
+                for (var i=0;i<listCategory.length;i++) {
+                    $('#'+selectName).append('<option value='  + listCategory[i].id +'>' + listCategory[i].name +'</option>');
+                }
+                $('#'+selectName).select2();
             },
             error: function () {
                 self.showDialog(BootstrapDialog.TYPE_WARNING,"Error","Ha ocurrido un error, intente nuevamente.");
