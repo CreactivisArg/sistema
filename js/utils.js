247567119fc6128b8d8afe3121bf553c628bd265
Sergio Araki <sergioaraki@gmail.com>
2015-08-13 22:35:56 -0300

    Mejorando el modal donde se cargan datos

--- js/utils.js
+++ js/utils.js
@@ -18,13 +18,11 @@ CTS.Utils = {
                 $('#'+selectName).empty();
 
                 for (var i=0;i<listStatus.length;i++) {
-                    var selected = "";
-
-                    if (id==listStatus[i].id)
-                        selected = "selected";
-
-                    $('#'+selectName).append('<option '+ selected +'  value='  + listStatus[i].id +'>' + listStatus[i].name +'</option>');
+                    $('#'+selectName).append('<option value='  + listStatus[i].id +'>' + listStatus[i].name +'</option>');
                 }
+                $('#'+selectName).select2();
+                if (id)
+                    $('#'+selectName).val(id).trigger("change");
             },
             error: function () {
                 self.showDialog(BootstrapDialog.TYPE_WARNING,"Error","Ha ocurrido un error, intente nuevamente.");
